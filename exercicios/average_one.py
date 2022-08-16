a1e72b9ba134485c40bba282b406b01f114eba9b
[bordignon-lucas] <[lucas.bordignon@edu.unifil.br]>
2022-08-15 22:04:36 -0300

    exercicios

---
+++ exercicios/average_one.py
@@ -0,0 +1,11 @@
+
+a = float(input())
+b = float(input())
+
+gradeWeightA = 3.5
+gradeWeightB = 7.5
+
+media = ((gradeWeightA * a) + (gradeWeightB * b)) / (gradeWeightA + gradeWeightB)
+
+print("MEDIA = %0.5f"%media)
+
